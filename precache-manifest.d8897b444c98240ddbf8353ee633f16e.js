a65042b2840fa15d1ee574d5f98e5e51d09356bd
Dmitriymush <powder322@gmail.com>
2019-10-30 03:40:14 +0200

    Updates

---
+++ precache-manifest.d8897b444c98240ddbf8353ee633f16e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce35c05a8f231013fb0cb776191ef48a",
+    "url": "/react_list-of-goods/index.html"
+  },
+  {
+    "revision": "e9c991ba5c92c02af580",
+    "url": "/react_list-of-goods/static/js/2.3cf6b9af.chunk.js"
+  },
+  {
+    "revision": "c0db0ea06a7a7b9132cb",
+    "url": "/react_list-of-goods/static/js/main.e9d7edad.chunk.js"
+  },
+  {
+    "revision": "e82fdff4b55e53116e45",
+    "url": "/react_list-of-goods/static/js/runtime~main.14fd7ef8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_list-of-goods/precache-manifest.d1a60a28eda3e5f773fa801cba116629.js"
+  "/react_list-of-goods/precache-manifest.d8897b444c98240ddbf8353ee633f16e.js"
 );
 
 self.addEventListener('message', (event) => {
